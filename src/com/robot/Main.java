8c78e406896379cee61c1d2e8beb289b5cf80bed
terry <terry@rokid.com>
2015-09-18 14:14:14 +0800

    try class

--- src/com/robot/Main.java
+++ src/com/robot/Main.java
@@ -1,8 +1,12 @@
 package com.robot;
 
+import test.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+        testclass tc = new testclass();
+        tc.sayhello();
     }
 }
---
+++ src/test/testclass.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by terry on 15/9/18.
+ */
+public class testclass {
+
+    public void sayhello() {
+        System.out.println("Hello Test!");
+    }
+}
