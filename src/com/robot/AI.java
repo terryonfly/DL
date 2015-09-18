40613d6b9479fbc8f2503be54d1143a7ef097782
terry <terry@rokid.com>
2015-09-18 14:36:48 +0800

    begin to code

--- src/com/robot/AI.java
+++ src/com/robot/AI.java
@@ -1,12 +1,11 @@
 package com.robot;
 
-import test.*;
-
-public class Main {
+/**
+ * Created by terry on 15/9/18.
+ */
+public class AI {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
-        testclass tc = new testclass();
-        tc.sayhello();
     }
 }
