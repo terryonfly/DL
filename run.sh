a174534574a90137453d19a7870cb91b8d649017
Terry <mykfc@qq.com>
2015-09-20 04:29:19 +0800

    change Xmx to max

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 javac -cp src/ -d bin/ src/com/robot/AI.java
-java -cp bin/:lib/mysql-connector-java-5.1.25-bin.jar com.robot.AI
+java -Xmx1774m -cp bin/:lib/mysql-connector-java-5.1.25-bin.jar com.robot.AI
