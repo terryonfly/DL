80a8abd99e87a120afb3ab24a57efe70f4f6fdb1
Terry <mykfc@qq.com>
2015-10-15 16:43:47 +0800

    change run.sh to 2048m memory

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 javac -cp src/ -d bin/ src/com/robot/AI.java
-java -Xmx1024m -cp bin/:lib/mysql-connector-java-5.1.25-bin.jar com.robot.AI
+java -Xmx2048m -cp bin/:lib/mysql-connector-java-5.1.25-bin.jar com.robot.AI
